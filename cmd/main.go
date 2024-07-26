62c0b2cee907f07a3a1d11375e9d52f7437f60f3
lijunliang <lijunliang@joyy.com>
2024-07-26 11:54:41 +0800

    feat: init

---
+++ cmd/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	response, err := http.Get("https://apidoc.inshopline.com/api/projectInterface/get?code=947b580d9c07c01497040071c7de2572")
+	if err != nil {
+		fmt.Println("Error sending request:", err)
+		return
+	}
+	defer response.Body.Close()
+	body, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	type Response struct {
+		Code    int    `json:"code"`
+		Message string `json:"message"`
+	}
+	var resp Response
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return
+	}
+	fmt.Println("Response:", resp)
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module tageedto
+
+go 1.22.2
