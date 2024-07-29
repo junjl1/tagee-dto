b21d51823797c1f38cd7793ad65be120f29bf6a5
Julian <lijunliang@joyy.com>
2024-07-29 16:03:52 +0800

    feat: types

--- fetcher/enter.go
+++ fetcher/enter.go
@@ -3,6 +3,7 @@ package fetcher
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/junjl1/tagee-dto/types"
 	"io/ioutil"
 	"net/http"
 	"strings"
@@ -14,12 +15,12 @@ func Fetch(tageeCode string) {
 	if tageeCode == "" {
 		url = ""
 	} else {
+		// 947b580d9c07c01497040071c7de2572
 		urlBuilder.WriteString("https://apidoc.inshopline.com/api/projectInterface/get?code=")
 		urlBuilder.WriteString(tageeCode)
 		url = urlBuilder.String()
 	}
 	response, err := http.Get(url)
-	//response, err := http.Get(`https://apidoc.inshopline.com/api/projectInterface/get?code=947b580d9c07c01497040071c7de2572`)
 	if err != nil {
 		fmt.Println("Error sending request:", err)
 		return
@@ -30,16 +31,11 @@ func Fetch(tageeCode string) {
 		fmt.Println("Error reading response body:", err)
 		return
 	}
-
-	type Response struct {
-		Code    int    `json:"code"`
-		Message string `json:"message"`
-	}
-	var resp Response
+	var resp types.Response
 	err = json.Unmarshal(body, &resp)
 	if err != nil {
 		return
 	}
-	fmt.Println("Response:", resp)
+	fmt.Println("Response:", resp.Data)
 	return
 }
