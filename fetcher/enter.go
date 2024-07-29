9280a710f7ea98cdd3687e5883f9b465b1945384
Julian <lijunliang@joyy.com>
2024-07-29 12:02:46 +0800

    feat: cobra init2

--- fetcher/enter.go
+++ fetcher/enter.go
@@ -5,10 +5,21 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strings"
 )
 
-func Fetch() {
-	response, err := http.Get("https://apidoc.inshopline.com/api/projectInterface/get?code=947b580d9c07c01497040071c7de2572")
+func Fetch(tageeCode string) {
+	var urlBuilder strings.Builder
+	var url string
+	if tageeCode == "" {
+		url = ""
+	} else {
+		urlBuilder.WriteString("https://apidoc.inshopline.com/api/projectInterface/get?code=")
+		urlBuilder.WriteString(tageeCode)
+		url = urlBuilder.String()
+	}
+	response, err := http.Get(url)
+	//response, err := http.Get(`https://apidoc.inshopline.com/api/projectInterface/get?code=947b580d9c07c01497040071c7de2572`)
 	if err != nil {
 		fmt.Println("Error sending request:", err)
 		return
