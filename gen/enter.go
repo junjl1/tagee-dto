bc0be86a52335f6e5b4e5741829b064e4731104f
Julian <lijunliang@joyy.com>
2024-07-30 15:40:52 +0800

    feat: demo 1

--- gen/enter.go
+++ gen/enter.go
@@ -1,21 +1,53 @@
 package gen
 
 import (
-	"fmt"
 	"github.com/dave/jennifer/jen"
+	"strings"
 )
 
-func GenDto() {
-	f := jen.NewFile("resDto")
+type Field struct {
+	structName string
+	fieldName  string
+	fieldType  string
+	comment    string
+}
 
-	//
-	f.Type().Id("Person").Struct(
-		jen.Id("Name").String().Tag(map[string]string{"json": "name"}).Comment("aaaa"),
-		jen.Id("Age").Int().Tag(map[string]string{"json": "age"}).Comment("bbbb"),
-	)
+type Generator struct {
+	f       *jen.File
+	structs map[string][]jen.Code
+}
 
-	err := f.Save("result.go")
-	if err != nil {
-		fmt.Errorf("failed to save file: %w", err)
+func (g *Generator) GetF() *jen.File {
+	return g.f
+}
+
+func NewGenerator(pkgName string) *Generator {
+	return &Generator{
+		f:       jen.NewFile(pkgName),
+		structs: make(map[string][]jen.Code),
 	}
+}
+
+func (g *Generator) SaveFile(fileName string) error {
+	return g.f.Save(fileName)
+}
+
+func (g *Generator) GenStruct(structName string) {
+	if _, exists := g.structs[structName]; !exists {
+		return
+	}
+	if fields, ok := g.structs[structName]; ok {
+		g.f.Type().Id(structName).Struct(fields...)
+	}
+}
+
+func (g *Generator) AppendField(structName string, fieldName string, fieldType string, comment string) {
+	if _, exists := g.structs[structName]; !exists {
+		g.structs[structName] = []jen.Code{
+			jen.Id(strings.ToUpper(fieldName)).Id(fieldType).Tag(map[string]string{"json": fieldName}).Comment(comment),
+		}
+		return
+	}
+	g.structs[structName] = append(g.structs[structName],
+		jen.Id(strings.ToUpper(fieldName)).Id(fieldType).Tag(map[string]string{"json": fieldName}).Comment(comment))
 }
--- internal/enter.go
+++ internal/enter.go
@@ -1,13 +1,45 @@
 package internal
 
 import (
+	"fmt"
+	"github.com/junjl1/tagee-dto/enum"
+	"github.com/junjl1/tagee-dto/fetcher"
 	"github.com/junjl1/tagee-dto/gen"
+	"strings"
 )
 
 func GenTask(tageeCode string) {
-	//data, err := fetcher.Fetch(tageeCode)
-	//if err != nil {
-	//	return
-	//}
-	gen.GenDto()
+	data, err := fetcher.Fetch(tageeCode)
+	if err != nil {
+		return
+	}
+	g := gen.NewGenerator("myPkgName")
+	for idx, value := range data.InputParam {
+		if idx == 0 && value.Key == "root" {
+			continue
+		}
+		parts := strings.Split(value.Key, ".")
+		structName := parts[len(parts)-1]
+		for _, dto := range value.DetailList {
+			t := enum.NewTageeType(dto.Type)
+			g.AppendField(structName, dto.Name, t.GetGoType(), dto.Comment)
+		}
+		g.GenStruct(structName)
+	}
+	err = g.SaveFile("res.go")
+	if err != nil {
+		fmt.Println("opps", err)
+		return
+	}
+	//typeIst := enum.NewTageeType("Integer")
+	//fmt.Println(typeIst.GetGoType())
+
+	//g := gen.NewGenerator("myPkgName")
+	//g.AppendField("Person", "name", "string", "person name")
+	//g.AppendField("Person", "age", "int", "person age")
+	//g.GenStruct("Person")
+	//g.AppendField("person", "name", "string", "person name")
+	//g.AppendField("person", "age", "Person", "person age")
+	//g.GenStruct("person")
+	//fmt.Printf("%#v", g.GetF())
 }
