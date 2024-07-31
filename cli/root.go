4249d35bd9ab1ac8c228bb5418a6f37e765af053
Julian <lijunliang@joyy.com>
2024-07-31 16:08:52 +0800

    chore: hidden version

--- cli/root.go
+++ cli/root.go
@@ -13,16 +13,16 @@ var (
 var rootCmd = &cobra.Command{
 	Use: "tagee-dto",
 	Run: func(cmd *cobra.Command, args []string) {
-		if cmd.Flags().Changed("version") {
-			fmt.Println("tagee-dto version", version)
-			return
-		}
+		//if cmd.Flags().Changed("version") {
+		//	fmt.Println("tagee-dto version", version)
+		//	return
+		//}
 	},
 }
 
 func Execute() {
 	rootCmd.CompletionOptions.HiddenDefaultCmd = true
-	rootCmd.Flags().BoolP("version", "v", false, "the version of tagee-dto")
+	//rootCmd.Flags().BoolP("version", "v", false, "the version of tagee-dto")
 	err := rootCmd.Execute()
 	if err != nil {
 		fmt.Println(err)
